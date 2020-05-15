45135c166e9576d4be424253868c4ce378a39064
fenix15100 <francisco.camacho15@gmail.com>
2020-05-15 03:48:55 +0200

    dsd

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def status():
 
 #Handles any requests that don't match the ones above and redirect to react static client route system
 @app.route("/", defaults={'path': ''})
-@app.route("/<path:path>/<string:path2>")
+@app.route("/<path:path>")
 @app.route("/<string:path>")
 def serve(path):
     return send_from_directory('./build','index.html')
