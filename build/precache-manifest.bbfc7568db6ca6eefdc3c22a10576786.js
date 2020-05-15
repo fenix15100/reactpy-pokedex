a90f2623f94a8056fad7d5bf8ddbc79867690c24
fenix15100 <francisco.camacho15@gmail.com>
2020-05-15 02:43:30 +0200

    add build folder to git

---
+++ build/precache-manifest.bbfc7568db6ca6eefdc3c22a10576786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace2aeceef50a02be4bc51b95f7b41fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91331527ced34ea55cc8",
+    "url": "/static/js/2.f751aff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f751aff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992e98efe160342d8419",
+    "url": "/static/js/main.f71e8183.chunk.js"
+  },
+  {
+    "revision": "f30f6164873b24ac3021",
+    "url": "/static/js/runtime-main.e7f41a92.js"
+  }
+]);
